e216be2bdf8a63e796ec0df63ae5c5926813c0e2
FrankDannyA <orgfrank@icloud.com>
2021-12-03 00:10:53 +0600

    Week
    Week

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var dayTF: UITextField!
+    
+    @IBOutlet weak var monthTF: UITextField!
+    
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        resultLabel.text = findDayOfTheWeek(day: Int(dayTF.text!), month: Int(monthTF.text!), year: Int(yearTF.text!))
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func findDayOfTheWeek(day: Int?, month: Int?, year: Int?) -> String{
+        
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        var dateComponents = DateComponents()
+        
+        guard let day = day, let month = month, let year = year else { return "Incorrect data" }
+        
+        if day > 31 || month > 12 {
+            return "Incorrect data"
+        }
+        
+        dateComponents.day = day
+        dateComponents.month = month
+        dateComponents.year = year
+        
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calendar.date(from: dateComponents)
+        let weekDay = dateFormatter.string(from: date!)
+        
+        return weekDay
+    }
+    
 }
 
